6970c05c6c735b8a6b31049d8bf98825225b6100
Raven <joe.javascript@gmail.com>
2024-02-04 23:56:35 +0400

    PERF: slightly less janky SSE live-reload handler

--- routes/live-reload.dev.js
+++ routes/live-reload.dev.js
@@ -2,6 +2,16 @@ import { liveReloadEmitter } from '../services/dev-server.js'
 
 const clients = []
 
+// TODO: huh, why nextTick?
+process.nextTick(() => {
+  liveReloadEmitter.on('reload', () => {
+    console.log('Reloading')
+    clients.forEach(client => {
+      client.write('data: Reload\n\n')
+    })
+  })
+})
+
 export default (req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/event-stream',
@@ -14,11 +24,6 @@ export default (req, res) => {
   req.on('close', () => {
     console.log('Client dropped me')
     clients.splice(clients.indexOf(res), 1)
-  })
-
-  // TODO: This is a memory leak
-  liveReloadEmitter.on('reload', () => {
-    res.write('data: Reload\n\n')
   })
 
   clients.push(res)

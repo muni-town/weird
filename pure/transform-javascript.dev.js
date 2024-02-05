060290ca1d2293813a5fbdc231fa07d144b2c01c
Raven <joe.javascript@gmail.com>
2024-02-05 13:02:53 +0400

    DEV: baseline for JSX transforms

--- pure/transform-javascript.dev.js
+++ pure/transform-javascript.dev.js
@@ -1,7 +1,18 @@
 import { transform as _transform } from '@babel/core'
 
 export default function transform(fileContents) {
-  const { code } = _transform(fileContents, {})
+  const { code } = _transform(fileContents, {
+    plugins: [
+      [
+        '@babel/plugin-transform-react-jsx',
+        {
+          pragma: 'JSXToString',
+          pragmaFrag: 'JSXFragmentToString',
+          throwIfNamespace: false
+        }
+      ]
+    ]
+  })
 
   return code
 }
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,14 @@ export default (req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/html'
   })
-  res.write('<h1>Welcome to the home page!</h1>')
+  res.write(
+    <div>
+      <h1>Hello, world!</h1>
+      <p>
+        This is a JSX file transformed to a
+        string.
+      </p>
+    </div>
+  )
   res.end()
 }

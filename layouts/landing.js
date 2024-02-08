348acba352eb56e704ab4663fb37053c5ded02a6
Raven <joe.javascript@gmail.com>
2024-02-08 11:29:11 +0400

    UX: basic username input on landing page

--- layouts/landing.js
+++ layouts/landing.js
@@ -11,18 +11,15 @@ export default () => (
       action='/actions/create-account'
       method='post'
     >
-      <input
-        type='text'
-        name='email'
-        placeholder='email'
-      />
-      <input
-        type='password'
-        name='password'
-        placeholder='password'
-      />
+      <div class='username-container'>
+        <input
+          placeholder='username'
+          class='username-input'
+        />
+        <p>.weird.one</p>
+      </div>
       <button type='submit'>
-        Create account
+        Create your page!
       </button>
     </form>
   </main>
@@ -33,6 +30,19 @@ function Style() {
     <style>{css`
       body {
         background-color: red;
+      }
+
+      .username-container {
+        background: rgb(255, 255, 255);
+        display: inline-grid;
+        grid-auto-flow: column;
+        place-items: start stretch;
+        grid-auto-columns: max-content;
+        grid-auto-rows: min-content;
+      }
+
+      .username-input {
+        border: 0px;
       }
     `}</style>
   )

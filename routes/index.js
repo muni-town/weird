b0f7bcf24f42e8f49d4971a678414889e9661aaf
Raven <joe.javascript@gmail.com>
2024-02-09 13:50:38 +0400

    DEV: relocates Rauthy data folder

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 import Document from '../layouts/document.js'
 import Landing from '../layouts/landing.js'
 
+// import { oAuthClients } from '../services/oauth-clients.js'
+
+// console.log(oAuthClients)
+
 export default (req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/html'

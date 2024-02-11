0511d14c21752116b9d4e305367795b0f90b309d
Raven <joe.javascript@gmail.com>
2024-02-11 17:41:27 +0400

    UX: dedicated create account form element

---
+++ elements/create-account-form.js
@@ -0,0 +1,64 @@
+export default (props, children) => (
+  <>
+    <Style />
+    <form
+      action='create-account'
+      method='post'
+      class='create-account-form'
+    >
+      <label for='username'>Username</label>
+      <input
+        type='text'
+        id='username'
+        name='username'
+      />
+      <label for='email'>Email</label>
+      <input
+        type='email'
+        id='email'
+        name='email'
+      />
+      <label for='password'>Password</label>
+      <input
+        type='password'
+        id='password'
+        name='password'
+      />
+      <button type='submit'>
+        Create Account
+      </button>
+    </form>
+  </>
+)
+
+function Style() {
+  return (
+    <style>{css`
+      .create-account-form {
+        display: grid;
+        background-color: red;
+        gap: 1rem;
+        padding: 1rem;
+        border: 1px solid #ccc;
+
+        label {
+          display: block;
+        }
+
+        input {
+          font-size: 1rem;
+          padding: 0.5rem;
+        }
+
+        button {
+          font-size: 1rem;
+          padding: 0.5rem 1rem;
+          background-color: #f60;
+          color: white;
+          border: none;
+          border-radius: 0.25rem;
+        }
+      }
+    `}</style>
+  )
+}
--- layouts/landing.js
+++ layouts/landing.js
@@ -3,52 +3,5 @@ export default () => (
     <Style />
     <h1>Weird.inc index</h1>
     <p>Welcome to the index page</p>
-
-    <form
-      action='create-account'
-      method='post'
-      enctype='application/x-www-form-urlencoded'
-      class='create-account-form'
-    >
-      <div class='container'>
-        <input
-          placeholder='username'
-          class='input'
-          name='username'
-        />
-        {/*honeypot*/}
-        {/*TODO: generate a random name for the honeypot field on server start*/}
-        <input
-          type='hidden'
-          name='honeypot'
-          value=''
-        />
-        <p>.weird.one</p>
-      </div>
-      <button type='submit'>
-        Create your page!
-      </button>
-    </form>
   </main>
 )
-
-function Style() {
-  return (
-    <style>{css`
-      .create-account-form {
-        .container {
-          background: rgb(255, 255, 255);
-          display: inline-grid;
-          grid-auto-flow: column;
-          place-items: start stretch;
-          grid-auto-columns: max-content;
-          grid-auto-rows: min-content;
-        }
-
-        .input {
-          border: 0px;
-        }
-      }
-    `}</style>
-  )
-}

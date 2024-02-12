e482817ce135c7810bdbdd6d507d3ce9c4d89b92
Raven <joe.javascript@gmail.com>
2024-02-12 18:36:34 +0400

    DEV: introduces a redis service

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ import Document from '../layouts/document.js'
 import Landing from '../layouts/landing.js'
 
 // import { oAuthClients } from '../services/oauth-clients.js'
+// import redisClient from '../services/redis.js'
+
+// console.log(redisClient)
 
 // console.log(oAuthClients)
 

6f7c948b52cdf28f1da2dc92b165eeead1176578
Raven <joe.javascript@gmail.com>
2024-02-13 20:35:44 +0400

    DEV: cleanup with no functional changes

--- layouts/landing.js
+++ layouts/landing.js
@@ -1,34 +1,8 @@
+import CreateAccountForm from '../elements/create-account-form.js'
+
 export default () => (
   <main>
-    <form
-      action='create-account'
-      method='post'
-    >
-      <label for='username'>Username:</label>
-      <input
-        type='text'
-        id='username'
-        name='username'
-      />
-      <br />
-      <label for='email'>Email:</label>
-      <input
-        type='email'
-        id='email'
-        name='email'
-      />
-      <br />
-      <label for='password'>Password:</label>
-      <input
-        type='password'
-        id='password'
-        name='password'
-      />
-      <br />
-      <button type='submit'>
-        Create Account
-      </button>
-    </form>
+    <CreateAccountForm />
 
     <h1>Weird.inc index</h1>
     <p>Welcome to the index page</p>

215c1348215d488dd0b68dac420d9f50ffe920a3
Raven <joe.javascript@gmail.com>
2024-02-17 20:03:34 +0400

    DEV: create account form cleanups

--- actions/create-account.js
+++ actions/create-account.js
@@ -3,6 +3,121 @@ import {
   getSession
 } from '../services/sessions.js'
 
+import { default as sql } from '../services/db-main.js'
+
+let id = 10
+
+const _createUser = async ({
+  username,
+  email,
+  password
+}) => {
+  const [createUserCode, createUserResult] =
+    await sql`
+      INSERT INTO weird_users
+      (username, email, password, id, weird_id)
+      VALUES
+      (${username}, ${email}, ${password}, ${id++}, ${
+      id++ + 1
+    })
+      RETURNING id
+    `
+  if (createUserCode > 0) {
+    throw new Error(createUserResult)
+  }
+
+  console.log(
+    'createUserResult',
+    createUserResult
+  )
+
+  const userId = createUserResult[0].id
+
+  return userId
+}
+
+const _createSession = async sessionData => {
+  const [createSessionCode, createSessionResult] =
+    await createSession({
+      sessionData
+    })
+
+  if (createSessionCode > 0) {
+    throw new Error(createSessionResult)
+  }
+
+  const sessionId = createSessionResult
+
+  return sessionId
+}
+
+const _getSession = async ({ sessionId }) => {
+  const [getSessionCode, getSessionResult] =
+    await getSession({
+      sessionId
+    })
+
+  if (getSessionCode > 0) {
+    throw new Error(getSessionResult)
+  }
+
+  const sessionData = getSessionResult
+
+  return sessionData
+}
+
+const handler = async context => {
+  const { res, formData } = context
+
+  try {
+    const { username, email, password } = formData
+
+    const userId = await _createUser({
+      username,
+      email,
+      password
+    })
+
+    const sessionId = await _createSession({
+      username,
+      userId
+    })
+
+    const sessionData = await _getSession({
+      sessionId
+    })
+
+    return (
+      <HttpResponse
+        res={res}
+        status={200}
+        headers={{ 'Content-Type': 'text/html' }}
+      >
+        <div>
+          <h1>Account Created</h1>
+          <p>
+            You submitted:
+            {JSON.stringify(formData)}
+          </p>
+          <p>Your session ID is: {sessionId}</p>
+          <p>
+            Your session data is:
+            {JSON.stringify(sessionData)}
+          </p>
+        </div>
+
+        {css`
+          h1 {
+            color: green;
+          }
+        `}
+      </HttpResponse>
+    )
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
 // TODO: this runs on every call. Should it? as a non-function it
 // needs JSX to be imported and not a global, but we want that anyway
 const Form = () => (
@@ -62,76 +177,4 @@ const Form = () => (
   </form>
 )
 
-const handler = async context => {
-  const { res, formData } = context
-
-  try {
-    const { username, email, password } = formData
-
-    const [
-      createSessionCode,
-      createSessionResult
-    ] = await createSession({
-      sessionData: {
-        username,
-        email,
-        password
-      }
-    })
-
-    if (createSessionCode > 0) {
-      throw new Error(createSessionResult)
-    }
-
-    const sessionId = createSessionResult
-
-    const [getSessionCode, getSessionResult] =
-      await getSession({
-        sessionId
-      })
-
-    if (getSessionCode > 0) {
-      throw new Error(getSessionResult)
-    }
-
-    const sessionData = getSessionResult
-
-    return (
-      <HttpResponse
-        res={res}
-        status={200}
-        headers={{ 'Content-Type': 'text/html' }}
-      >
-        <div>
-          <h1>Account Created</h1>
-          <p>
-            You submitted:
-            {JSON.stringify(formData)}
-          </p>
-          <p>Your session ID is: {sessionId}</p>
-          <p>
-            Your session data is:
-            {JSON.stringify(sessionData)}
-          </p>
-        </div>
-
-        {css`
-          h1 {
-            color: green;
-          }
-        `}
-      </HttpResponse>
-    )
-  } catch (error) {
-    return (
-      <HttpResponse
-        res={res}
-        error={error}
-        status={500}
-        message={`error parsing form data: ${error.message}`}
-      />
-    )
-  }
-}
-
-export { Form, handler }
+export { handler, Form }

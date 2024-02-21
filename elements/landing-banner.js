c71ae1370ebadd2a7344c3fc47be1b8526eb6751
Raven <joe.javascript@gmail.com>
2024-02-21 04:13:31 +0400

    UX: adds landing banner element

---
+++ elements/landing-banner.js
@@ -0,0 +1,26 @@
+export default (props, children) => (
+  <>
+    <div class='landing-form'>
+      <div>
+        <form action='/actions/create-account'>
+          <input
+            type='text'
+            name='username'
+            placeholder='Username'
+          />
+          <span>.weird.one</span>
+        </form>
+      </div>
+    </div>
+
+    {css`
+      .landing-form {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background: #f8f8f8;
+      }
+    `}
+  </>
+)

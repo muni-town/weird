396b84deb7602edd913952c8444ec42d921f8714
Zicklag <zicklag@katharostech.com>
2024-05-07 11:32:49 -0500

    feat: add Rauthy docker compose setup and access that from Svelte app.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<h1>Key Info</h1>
+
+<pre>
+    {JSON.stringify(data.key_info)}
+</pre>

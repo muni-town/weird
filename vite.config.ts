3ce6076fa5b9a9e32a4bc8e4affa1831900630f6
Zicklag <zicklag@katharostech.com>
2024-05-11 19:19:34 -0500

    refactor: install skeleton UI framework instead of UnoCSS.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import UnoCSS from 'unocss/vite';
-import extractorSvelte from '@unocss/extractor-svelte';
 
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
-		UnoCSS({
-			extractors: [extractorSvelte()]
-		}),
 		sveltekit()
 	]
 });

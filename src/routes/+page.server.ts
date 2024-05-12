28f768ec33a30f1ddae33b234e0d0252470ce7e3
Zicklag <zicklag@katharostech.com>
2024-05-12 09:22:11 -0500

    feat: get key info loading properly from proxied Rauthy.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,11 @@
 import { env } from '$env/dynamic/private';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ cookies }) => {
-	const headers = new Headers({
-		'Set-Cookie': cookies.toString()
-	});
-	const resp = await fetch(env.RAUTHY_URL + '/auth/v1/auth_check', { headers });
+export const load: PageServerLoad = async ({ request }) => {
+	const headers = new Headers();
+	const cookies = request.headers.get('Cookie');
+	cookies ? headers.set('Cookie', cookies) : undefined;
+	const resp = await fetch(env.RAUTHY_URL + '/auth/v1/login_time', { headers });
 	const key_info = JSON.parse(await resp.text());
 
 	return { key_info };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,14 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
 <main class="flex-col justify-center p-4 text-center">
-	<h1 class="text-4xl font-bold mt-8">Weird Web Pages</h1>
-	<p class="text-lg mt-4">Materializing the web from first principles.</p>
+	<h1 class="mt-8 text-4xl font-bold">Weird Web Pages</h1>
+	<p class="mt-4 text-lg">Materializing the web from first principles.</p>
+
+	<div class="mt-8">
+		{JSON.stringify(data)}
+	</div>
 </main>

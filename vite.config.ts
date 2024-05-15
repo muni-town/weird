3705aa93703bb6c771172c5b6c72f4dc56db7c13
Zicklag <zicklag@katharostech.com>
2024-05-14 21:18:23 -0500

    fix: fix production build by installing vite-plugin-top-level-await.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [wasm(), sveltekit()]
+	plugins: [wasm(), topLevelAwait(), sveltekit()]
 });

836e35dc2bc77664198146362c79b3d536bf33b5
Zicklag <zicklag@katharostech.com>
2024-05-14 21:18:23 -0500

    fix: fix production build by installing vite-plugin-top-level-await.

--- src/routes/auth/v1/users/[user]/reset/[token]/+page.svelte
+++ src/routes/auth/v1/users/[user]/reset/[token]/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	// import type { PageData } from './$types';
 
-	export let data: PageData;
+	// export let data: PageData;
 </script>
 
 <main class="flex flex-col items-center">
 	<form method="POST" class="card mt-12 flex w-[600px] max-w-[90%] flex-col gap-4 p-8">
-		<h1 class="my-3 text-2xl">Reset Password</h1>
-		<p>You have requested a password reset. Input your new password now.</p>
+		<h1 class="my-3 text-2xl">Set Password</h1>
+		<p>Set a new password for </p>
 		<label class="label">
 			<span>Password</span>
 			<input name="password" class="input" type="text" placeholder="Password" />

aab418c7d96dd61b78a0923ba073d775ec309e48
Zicklag <zicklag@katharostech.com>
2024-05-17 09:17:32 -0500

    refactor: start to copy the way rauthy's UI does login more closely.

---
+++ src/routes/auth/v1/+page.svelte
@@ -0,0 +1,6 @@
+<div class="flex justify-center">
+	<div class="flex flex-col gap-4 mt-8">
+		<a href="/auth/v1/account" class="variant-ghost btn">Login</a>
+		<a href="/auth/v1/users/register" class="variant-ghost btn">Register</a>
+	</div>
+</div>
---
+++ src/routes/auth/v1/oidc/authorize/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	let showPassword = $state(false);
+</script>
+
+<main class="flex flex-col items-center">
+	<form class="card mt-12 flex w-[600px] max-w-[90%] flex-col gap-4 p-8">
+		<h1 class="my-3 text-2xl">Login</h1>
+		<label class="label">
+			<span>Email</span>
+			<input name="email" class="input" type="text" placeholder="Email" />
+		</label>
+
+		{#if showPassword}
+			<label class="label">
+				<span>Password</span>
+				<input name="password" class="input" type="password" placeholder="Password" />
+			</label>
+		{/if}
+
+		<div class="mt-4 flex flex-col gap-2">
+			<p class="text-center">
+				Don't have an account?
+				<a href="/account/register" class="underline">Register a new account</a>.
+			</p>
+
+			<button class="variant-filled btn"> Login </button>
+		</div>
+	</form>
+</main>

5c92cb23ed93bb0afafa89a8b781d900dbffa621
Zicklag <zicklag@katharostech.com>
2024-05-17 21:30:18 -0500

    feat: implement logout button and user session awareness in the app.

--- src/lib/components/layouts/DefaultLayout.svelte
+++ src/lib/components/layouts/DefaultLayout.svelte
@@ -1,16 +1,26 @@
 <script>
+	import { getUserInfo } from '$lib/rauthy';
 	import { LightSwitch } from '@skeletonlabs/skeleton';
 	import { AppBar } from '@skeletonlabs/skeleton';
+
+	const userInfo = getUserInfo();
 </script>
 
 <AppBar>
-	<svelte:fragment slot="lead"><img src="/logo.png" alt="Weird Logo" width="40px" /></svelte:fragment>
+	<svelte:fragment slot="lead"
+		><img src="/logo.png" alt="Weird Logo" width="40px" /></svelte:fragment
+	>
 	<svelte:fragment slot="default"
 		><h1 class="text-xl font-bold"><a href="/">Weird.one</a></h1></svelte:fragment
 	>
 	<svelte:fragment slot="trail">
-		<a href="/auth/v1/account" class="btn variant-ghost">Login</a>
-		<a href="/auth/v1/users/register" class="btn variant-ghost">Register</a>
+		{#if !userInfo}
+			<a href="/auth/v1/account" class="variant-ghost btn">Login</a>
+			<a href="/auth/v1/users/register" class="variant-ghost btn">Register</a>
+		{:else}
+			<a href="/auth/v1/account" class="variant-ghost btn">Account</a>
+			<a href="/auth/v1/oidc/logout" class="variant-ghost btn">Logout</a>
+		{/if}
 		<LightSwitch />
 	</svelte:fragment>
 </AppBar>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
-<script>
+<script lang="ts">
+	import type { LayoutData } from './$types';
 	import DefaultLayout from '$lib/components/layouts/DefaultLayout.svelte';
 	import '../app.css';
+	import { setSessionInfo, setUserInfo } from '$lib/rauthy';
+
+	const { data }: { data: LayoutData } = $props();
+	setSessionInfo(data.sessionInfo);
+	setUserInfo(data.userInfo);
 </script>
 
 <DefaultLayout>

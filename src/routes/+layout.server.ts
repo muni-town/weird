6a621c5ae503bfd8551e73308804da60c37245b3
Zicklag <zicklag@katharostech.com>
2024-05-24 13:16:42 -0500

    feat: fetch user info on the server to hide session detection glitch.

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,26 @@
+import { checkResponse } from '$lib/utils';
+import type { LayoutServerLoad } from './$types';
+import type { SessionInfo, UserInfo } from '$lib/rauthy';
+
+export const load: LayoutServerLoad = async ({ fetch, cookies }) => {
+	let sessionInfo: SessionInfo | undefined = undefined;
+	let userInfo: UserInfo | undefined = undefined;
+
+	const rauthySession = cookies.get('RauthySession');
+
+	try {
+		const sessionInfoResp = await fetch('/auth/v1/oidc/sessioninfo', {
+			headers: [['Cookie', `RauthySession=${rauthySession}`]]
+		});
+		await checkResponse(sessionInfoResp);
+		sessionInfo = await sessionInfoResp.json();
+
+		const userInfoResp = await fetch(`/auth/v1/users/${sessionInfo?.user_id}`, {
+			headers: [['Cookie', `RauthySession=${rauthySession}`]]
+		});
+		await checkResponse(userInfoResp);
+		userInfo = await userInfoResp.json();
+	} catch (_) {}
+
+	return { sessionInfo, userInfo };
+};

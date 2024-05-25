4d8abcfbd3771de8f56f35442ca808c403051874
Zicklag <zicklag@katharostech.com>
2024-05-25 10:18:31 -0500

    fix: try to fix production deployment by reading RauthyUser cookie.

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -9,16 +9,17 @@ export const load: LayoutServerLoad = async ({ fetch, cookies }) => {
 	let userInfo: UserInfo | undefined = undefined;
 
 	const rauthySession = cookies.get(`${env.PUBLIC_COOKIE_PREFIX}RauthySession`);
+	const rauthyUser = cookies.get(`${env.PUBLIC_COOKIE_PREFIX}RauthyUser`);
 
 	try {
 		const sessionInfoResp = await fetch('/auth/v1/oidc/sessioninfo', {
-			headers: [['Cookie', `${env.PUBLIC_COOKIE_PREFIX}RauthySession=${rauthySession}`]]
+			headers: [['Cookie', `${env.PUBLIC_COOKIE_PREFIX}RauthySession=${rauthySession};${env.PUBLIC_COOKIE_PREFIX}RauthyUser=${rauthyUser}`]]
 		});
 		await checkResponse(sessionInfoResp);
 		sessionInfo = await sessionInfoResp.json();
 
 		const userInfoResp = await fetch(`/auth/v1/users/${sessionInfo?.user_id}`, {
-			headers: [['Cookie', `${env.PUBLIC_COOKIE_PREFIX}RauthySession=${rauthySession}`]]
+			headers: [['Cookie', `${env.PUBLIC_COOKIE_PREFIX}RauthySession=${rauthySession};${env.PUBLIC_COOKIE_PREFIX}RauthyUser=${rauthyUser}`]]
 		});
 		await checkResponse(userInfoResp);
 		userInfo = await userInfoResp.json();

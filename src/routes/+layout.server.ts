0fa1d7cd250a512911b7ffd39a589017d2a46ec5
Zicklag <zicklag@katharostech.com>
2024-05-25 10:57:44 -0500

    fix: proxy all headers when getting user session on the server.

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -4,29 +4,22 @@ import type { LayoutServerLoad } from './$types';
 import type { SessionInfo, UserInfo } from '$lib/rauthy';
 
 // TODO: Move this logic to a "SessionProvider" component.
-export const load: LayoutServerLoad = async ({ fetch, cookies }) => {
+export const load: LayoutServerLoad = async ({ fetch, request }) => {
 	let sessionInfo: SessionInfo | undefined = undefined;
 	let userInfo: UserInfo | undefined = undefined;
 
-	const rauthySession = cookies.get(`${env.PUBLIC_COOKIE_PREFIX}RauthySession`);
-	console.log(rauthySession);
-
 	try {
 		const sessionInfoResp = await fetch('/auth/v1/oidc/sessioninfo', {
-			headers: [['Cookie', `${env.PUBLIC_COOKIE_PREFIX}RauthySession=${rauthySession}`]]
+			headers: request.headers
 		});
-		console.log(sessionInfoResp);
 		await checkResponse(sessionInfoResp);
 		sessionInfo = await sessionInfoResp.json();
-		console.log(sessionInfo);
 
 		const userInfoResp = await fetch(`/auth/v1/users/${sessionInfo?.user_id}`, {
-			headers: [['Cookie', `${env.PUBLIC_COOKIE_PREFIX}RauthySession=${rauthySession}`]]
+			headers: request.headers
 		});
-		console.log(userInfoResp)
 		await checkResponse(userInfoResp);
 		userInfo = await userInfoResp.json();
-		console.log(userInfo);
 	} catch (_) {}
 
 	return { sessionInfo, userInfo };

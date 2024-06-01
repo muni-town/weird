6d3ae18a0b55263684861d448fe3baa0f0df6daa
Zicklag <zicklag@katharostech.com>
2024-06-01 15:23:07 -0500

    feat: add placeholder members page and mobile drawer menu.

--- src/lib/components/layouts/DefaultLayout.svelte
+++ src/lib/components/layouts/DefaultLayout.svelte
@@ -2,8 +2,15 @@
 	import { getUserInfo } from '$lib/rauthy';
 	import { LightSwitch } from '@skeletonlabs/skeleton';
 	import { AppBar } from '@skeletonlabs/skeleton';
+	import { getDrawerStore } from '@skeletonlabs/skeleton';
 
 	const userInfo = getUserInfo();
+
+	const drawerStore = getDrawerStore();
+
+	const openDrawer = () => {
+		drawerStore.open();
+	};
 </script>
 
 <AppBar>
@@ -14,14 +21,34 @@
 		><h1 class="text-xl font-bold"><a href="/">Weird.one</a></h1></svelte:fragment
 	>
 	<svelte:fragment slot="trail">
-		{#if !userInfo}
-			<a href="/auth/v1/account" class="variant-ghost btn">Login</a>
-			<a href="/auth/v1/users/register" class="variant-ghost btn">Register</a>
-		{:else}
-			<a href="/auth/v1/account" class="variant-ghost btn">Profile</a>
-			<a href="/auth/v1/oidc/logout" class="variant-ghost btn">Logout</a>
-		{/if}
-		<LightSwitch />
+		<div class="hidden items-center gap-3 sm:flex">
+			<a href="/members" class="variant-ghost btn">Members</a>
+			{#if !userInfo}
+				<a href="/auth/v1/account" class="variant-ghost btn">Login</a>
+				<a href="/auth/v1/users/register" class="variant-ghost btn">Register</a>
+			{:else}
+				<a href="/auth/v1/account" class="variant-ghost btn">Profile</a>
+				<a href="/auth/v1/oidc/logout" class="variant-ghost btn">Logout</a>
+			{/if}
+			<LightSwitch />
+		</div>
+		<div class="sm:hidden">
+			<button class="variant-ghost btn-icon" onclick={openDrawer}>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					fill="currentColor"
+					class="bi bi-list"
+					viewBox="0 0 16 16"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
+					/>
+				</svg>
+			</button>
+		</div>
 	</svelte:fragment>
 </AppBar>
 
---
+++ src/routes/members/+page.svelte
@@ -0,0 +1,5 @@
+<main class="flex flex-col items-center">
+    <h1 class="text-4xl font-bold mt-8">Weird Ones</h1>
+
+    <div class="mt-8">Coming soon...</div>
+</main>

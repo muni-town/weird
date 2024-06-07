efd7c8a077e1646aeb5bba2d144bda166c528a0d
Zicklag <zicklag@katharostech.com>
2024-06-07 12:15:01 -0500

    feat: implement generated user avatars.

---
+++ src/lib/components/Avatar.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { env } from '$env/dynamic/public';
+	import { Avatar } from '@skeletonlabs/skeleton';
+
+	const baseUrl = env.PUBLIC_DICEBEAR_URL + '/8.x/bottts/svg?seed=';
+
+	let { username }: { username: string } = $props();
+</script>
+
+<Avatar
+	background="bg-transparent"
+	src={`${baseUrl}${username}`}
+	rounded="rounded-full"
+	width="w-32"
+/>
---
+++ src/routes/u/[username]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Avatar from '$lib/components/Avatar.svelte';
+	import type { PageData } from './$types';
+	const { data }: { data: PageData } = $props();
+</script>
+
+{#if data.profile}
+	<main class="flex flex-col items-center">
+		<div class="card mt-12 flex w-[600px] max-w-[90%] flex-col gap-4 p-8 text-xl">
+			<div class="flex items-center gap-4">
+				<Avatar username={data.profile.avatar_seed || ''} />
+				<h1 class="my-3 text-4xl">{data.profile.username}</h1>
+			</div>
+		</div>
+	</main>
+{/if}

02396393a67d18e272d3c51c5e6d5f87dd4f7685
Zicklag <zicklag@katharostech.com>
2024-06-13 13:50:48 -0500

    feat: add config for instance name and members title.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,9 @@
+<script>
+
+	import { env } from "$env/dynamic/public";
+
+</script>
 <main class="flex-col justify-center p-4 text-center">
-	<h1 class="mt-8 text-4xl font-bold">Weird Web Pages</h1>
+	<h1 class="mt-8 text-4xl font-bold">{env.PUBLIC_INSTANCE_NAME}</h1>
 	<p class="mt-4 text-lg">Materializing the web from first principles.</p>
 </main>

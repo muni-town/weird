1c25094eb8f704b407751fe13336c02c41223863
Zicklag <zicklag@katharostech.com>
2024-06-15 15:44:14 -0500

    feat: show instance name in feedback message.

--- src/routes/feedback/+page.server.ts
+++ src/routes/feedback/+page.server.ts
@@ -2,13 +2,17 @@ import { redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
 
 import { env } from '$env/dynamic/private';
+import { env as pubenv } from '$env/dynamic/public';
 
 export const actions = {
 	default: async ({ fetch, request }) => {
 		const data = await request.formData();
 		await fetch(env.FEEDBACK_WEBHOOK, {
 			method: 'post',
-			body: data
+			body: JSON.stringify({
+				username: `${pubenv.PUBLIC_INSTANCE_NAME} ( ${(data.get('email') || 'Anonymous').toString()} )`,
+				content: (data.get('content') || 'empty message')?.toString()
+			})
 		});
 
 		return redirect(302, '/feedback/confirmation');

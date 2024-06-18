3eedadb5ffe7a52fb1b774481311e3926b1e8cdf
Zicklag <zicklag@katharostech.com>
2024-06-18 15:52:51 -0500

    feat: implement semi-stable DB export/import.

--- backend/src/routes/db.rs
+++ backend/src/routes/db.rs
@@ -1,11 +1,12 @@
 use std::{fmt::Display, str::FromStr};
 
-use gdata::{IrohGStore, Key, Value};
+use gdata::{GStoreBackend, IrohGStore, Key, Value};
 use iroh::{
     base::node_addr::AddrInfoOptions,
     client::docs::ShareMode,
     docs::{Author, Capability, CapabilityKind},
 };
+use profile::Profile;
 
 use crate::utils::Yaml;
 
@@ -19,149 +20,72 @@ pub fn install(router: Router<AppState>) -> Router<AppState> {
         .route("/db/raw/import", post(db_raw_import))
 }
 
-#[derive(Serialize, Deserialize)]
-struct ImportExportFormat {}
+/// Export database to a semi-stable [`ImportExportFormat`].
+pub async fn db_export(state: State<AppState>) -> AppResult<Yaml<ImportExportFormat>> {
+    let mut export = ImportExportFormat::default();
 
-pub async fn db_export(state: State<AppState>) -> AppResult<Json<HashMap<String, String>>> {
-    let doc = state.node.docs().open(state.ns).await?.unwrap();
-    let mut map = HashMap::default();
-    let mut stream = doc.get_many(Query::all()).await?;
-    while let Some(result) = stream.next().await {
-        let entry = result?;
-        map.insert(
-            String::from_utf8_lossy(entry.key()).into_owned(),
-            String::from_utf8_lossy(&entry.content_bytes(&doc).await?).into_owned(),
-        );
-    }
-    Ok(Json(map))
-}
+    let author = state
+        .node
+        .authors()
+        .export(state.node_author)
+        .await?
+        .ok_or_else(|| anyhow::format_err!("Missing author"))?;
 
-pub async fn db_import() -> AppResult<()> {
-    Ok(())
-}
-
-/// String-based serialize/deserialize wrapper.
-#[derive(Debug, Clone)]
-pub struct StringSerde<T>(pub T);
-impl<T: std::fmt::Display> serde::Serialize for StringSerde<T> {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        serializer.serialize_str(&format!("{}", self.0))
-    }
-}
-impl<'de, E: Display, T: FromStr<Err = E>> serde::Deserialize<'de> for StringSerde<T> {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        use serde::de::Error;
-        let s = String::deserialize(deserializer)?;
-        Ok(Self(
-            s.parse().map_err(|e| D::Error::custom(format!("{e}")))?,
-        ))
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct SerdeCapability(pub Capability);
-impl serde::Serialize for SerdeCapability {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        match &self.0 {
-            Capability::Write(ns_secret) => serializer.serialize_newtype_variant(
-                "Capability",
-                0,
-                "Write",
-                &StringSerde(ns_secret),
-            ),
-            Capability::Read(ns_id) => {
-                serializer.serialize_newtype_variant("Capability", 0, "Read", &StringSerde(ns_id))
-            }
-        }
-    }
-}
-impl<'de> serde::Deserialize<'de> for SerdeCapability {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        deserializer.deserialize_enum("Capability", &["Write", "Read"], CapabilityVisitor)
-    }
-}
-struct CapabilityVisitor;
-impl<'de> serde::de::Visitor<'de> for CapabilityVisitor {
-    type Value = SerdeCapability;
-    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(formatter, "read or write capability")
+    let profiles = state.graph.get((state.ns, "profiles")).await?;
+    let mut stream = profiles.list_items().await?;
+    while let Some(profile) = stream.next().await {
+        let value = profile?;
+        let profile = Profile::from_value(&value).await?;
+        export.profiles.push(ProfileWrapper {
+            author: StringSerde(author.clone()),
+            rauthy_user_id: value
+                .link
+                .key
+                .clone()
+                .last()
+                .unwrap()
+                .as_str()
+                .ok_or_else(|| anyhow::format_err!("User ID not string"))?
+                .to_string(),
+            info: profile,
+        });
     }
 
-    fn visit_enum<A>(self, access: A) -> Result<Self::Value, A::Error>
-    where
-        A: serde::de::EnumAccess<'de>,
-    {
-        use serde::de::{Error, VariantAccess};
-        let (variant_name, variant) = access.variant::<String>()?;
-        let variant_data = variant.newtype_variant::<String>()?;
-        match variant_name.as_str() {
-            "Write" => Ok(SerdeCapability(Capability::Write(
-                variant_data
-                    .parse()
-                    .map_err(|e| A::Error::custom(format!("{e}")))?,
-            ))),
-            "Read" => Ok(SerdeCapability(Capability::Read(
-                variant_data
-                    .parse()
-                    .map_err(|e| A::Error::custom(format!("{e}")))?,
-            ))),
-            _ => Err(A::Error::custom(
-                "Unrecognized enum variant, expected \"Write\" or \"Read\"",
-            )),
-        }
+    Ok(Yaml(export))
+}
+
+/// Import database from the semi-stable [`ImportExportFormat`].
+pub async fn db_import(
+    state: State<AppState>,
+    data: Yaml<ImportExportFormat>,
+) -> AppResult<Yaml<RawImportExportFormat>> {
+    // Export the data before importing it, just in case the import corrupts something.
+    let export = db_raw_export(state.clone()).await?;
+
+    let profiles = state.graph.get_or_init_map((state.ns, "profiles")).await?;
+
+    // Clear profiles
+    profiles.del_all_keys().await?;
+
+    // Import namespaces
+    for profile in data.0.profiles {
+        state.node.authors().import(profile.author.0).await?;
+
+        let value = profiles.get_key_or_init_map(profile.rauthy_user_id).await?;
+        profile.info.write_to_value(&value).await?;
     }
+
+    Ok(export)
 }
 
-/// This is a raw key-value export of the database graph.
-///
-/// This stands in contrast to the [`ImportExportFormat`] which is meant to be a more stable
-/// representation of the profile data that we store, that can be used to help facilitate migrations
-/// and backups.
-///
-/// This format is meant more for debugging.
-#[derive(Serialize, Deserialize, Debug, Default)]
-pub struct RawImportExportFormat {
-    pub authors: Vec<StringSerde<Author>>,
-    pub namespaces: Vec<RawImportExportNamespace>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct RawImportExportNamespace {
-    pub capability: SerdeCapability,
-    pub records: Vec<RawImportExportRecord>,
-}
-impl RawImportExportNamespace {
-    pub fn new(capability: Capability) -> Self {
-        Self {
-            capability: SerdeCapability(capability),
-            records: Default::default(),
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct RawImportExportRecord {
-    key: Key,
-    value: Value,
-    author: StringSerde<AuthorId>,
-}
-
+/// Import raw database entries from the unstable [`RawImportExportFormat`].
 pub async fn db_raw_import(
     state: State<AppState>,
     data: Yaml<RawImportExportFormat>,
-) -> AppResult<()> {
+) -> AppResult<Yaml<RawImportExportFormat>> {
+    // Export the data before importing it, just in case the import corrupts something.
+    let export = db_raw_export(state.clone()).await?;
+
     // Drop all documents
     let mut docs = state.node.docs().list().await?;
     while let Some(doc) = docs.next().await {
@@ -193,8 +117,10 @@ pub async fn db_raw_import(
         }
     }
 
-    Ok(())
+    Ok(export)
 }
+
+/// Export raw database entries to the unstable [`RawImportExportFormat`].
 pub async fn db_raw_export(state: State<AppState>) -> AppResult<Yaml<RawImportExportFormat>> {
     let mut export = RawImportExportFormat::default();
 
@@ -243,4 +169,147 @@ pub async fn db_raw_export(state: State<AppState>) -> AppResult<Yaml<RawImportEx
     }
 
     Ok(Yaml(export))
+}
+
+use self::format::*;
+mod format {
+    use super::*;
+
+    #[derive(Serialize, Deserialize, Default)]
+    pub struct ImportExportFormat {
+        pub profiles: Vec<ProfileWrapper>,
+    }
+
+    #[derive(Serialize, Deserialize)]
+    pub struct ProfileWrapper {
+        pub author: StringSerde<Author>,
+        pub rauthy_user_id: String,
+        pub info: Profile,
+    }
+
+    /// This is a raw key-value export of the database graph.
+    ///
+    /// This stands in contrast to the [`ImportExportFormat`] which is meant to be a more stable
+    /// representation of the profile data that we store, that can be used to help facilitate migrations
+    /// and backups.
+    ///
+    /// This format is meant more for debugging.
+    #[derive(Serialize, Deserialize, Debug, Default)]
+    pub struct RawImportExportFormat {
+        pub authors: Vec<StringSerde<Author>>,
+        pub namespaces: Vec<RawImportExportNamespace>,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct RawImportExportNamespace {
+        pub capability: SerdeCapability,
+        pub records: Vec<RawImportExportRecord>,
+    }
+    impl RawImportExportNamespace {
+        pub fn new(capability: Capability) -> Self {
+            Self {
+                capability: SerdeCapability(capability),
+                records: Default::default(),
+            }
+        }
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct RawImportExportRecord {
+        pub key: Key,
+        pub value: Value,
+        pub author: StringSerde<AuthorId>,
+    }
+}
+
+use self::ser_de::*;
+mod ser_de {
+    use super::*;
+
+    /// String-based serialize/deserialize wrapper.
+    #[derive(Debug, Clone)]
+    pub struct StringSerde<T>(pub T);
+    impl<T: std::fmt::Display> serde::Serialize for StringSerde<T> {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            serializer.serialize_str(&format!("{}", self.0))
+        }
+    }
+    impl<'de, E: Display, T: FromStr<Err = E>> serde::Deserialize<'de> for StringSerde<T> {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: serde::Deserializer<'de>,
+        {
+            use serde::de::Error;
+            let s = String::deserialize(deserializer)?;
+            Ok(Self(
+                s.parse().map_err(|e| D::Error::custom(format!("{e}")))?,
+            ))
+        }
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct SerdeCapability(pub Capability);
+    impl serde::Serialize for SerdeCapability {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            match &self.0 {
+                Capability::Write(ns_secret) => serializer.serialize_newtype_variant(
+                    "Capability",
+                    0,
+                    "Write",
+                    &StringSerde(ns_secret),
+                ),
+                Capability::Read(ns_id) => serializer.serialize_newtype_variant(
+                    "Capability",
+                    0,
+                    "Read",
+                    &StringSerde(ns_id),
+                ),
+            }
+        }
+    }
+    impl<'de> serde::Deserialize<'de> for SerdeCapability {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: serde::Deserializer<'de>,
+        {
+            deserializer.deserialize_enum("Capability", &["Write", "Read"], CapabilityVisitor)
+        }
+    }
+    struct CapabilityVisitor;
+    impl<'de> serde::de::Visitor<'de> for CapabilityVisitor {
+        type Value = SerdeCapability;
+        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+            write!(formatter, "read or write capability")
+        }
+
+        fn visit_enum<A>(self, access: A) -> Result<Self::Value, A::Error>
+        where
+            A: serde::de::EnumAccess<'de>,
+        {
+            use serde::de::{Error, VariantAccess};
+            let (variant_name, variant) = access.variant::<String>()?;
+            let variant_data = variant.newtype_variant::<String>()?;
+            match variant_name.as_str() {
+                "Write" => Ok(SerdeCapability(Capability::Write(
+                    variant_data
+                        .parse()
+                        .map_err(|e| A::Error::custom(format!("{e}")))?,
+                ))),
+                "Read" => Ok(SerdeCapability(Capability::Read(
+                    variant_data
+                        .parse()
+                        .map_err(|e| A::Error::custom(format!("{e}")))?,
+                ))),
+                _ => Err(A::Error::custom(
+                    "Unrecognized enum variant, expected \"Write\" or \"Read\"",
+                )),
+            }
+        }
+    }
 }

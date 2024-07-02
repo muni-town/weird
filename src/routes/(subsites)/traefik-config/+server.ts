5fb76d7be5939b4e0e00f7907cfc7874d6a98d8c
Zicklag <zicklag@katharostech.com>
2024-07-02 08:53:41 -0500

    feat: add dynamic traefik config.

--- src/routes/(subsites)/traefik-config/+server.ts
+++ src/routes/(subsites)/traefik-config/+server.ts
@@ -1,9 +1,36 @@
+import { env } from '$env/dynamic/private';
+import { env as pubenv } from '$env/dynamic/public';
+import { backendFetch } from '$lib/backend';
+import { checkResponse } from '$lib/utils';
 import { json, type RequestHandler } from '@sveltejs/kit';
 
-export const GET: RequestHandler = () => {
-	return json({
-		http: {
-			services: {}
+export const GET: RequestHandler = async ({ fetch }) => {
+	try {
+		const resp = await backendFetch(fetch, '/usernames');
+		await checkResponse(resp);
+		const usernames: string[] = Object.keys(await resp.json()).map((x) => x.split('@')[0]);
+
+		const routers: {
+			[key: string]: { rule: string; tls?: { certResolver: string }; service: string };
+		} = {};
+		for (const username of usernames) {
+			const routerName = `${env.TRAEFIK_CONFIG_NAMESPACE}-rtr-${username}`;
+			routers[routerName] = {
+				rule: `Host(${username}.${pubenv.PUBLIC_DOMAIN})`,
+				service: env.TRAEFIK_CONFIG_SERVICE_NAME,
+				tls: {
+					certResolver: 'letsencrypt'
+				}
+			};
 		}
-	});
+
+		return json({
+			http: {
+				routers
+			}
+		});
+	} catch (e) {
+		console.error('Error loading usernames while creating traefik config', e);
+		return json({});
+	}
 };

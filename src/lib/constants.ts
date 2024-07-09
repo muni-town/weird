8346275294057ea41f3c6c37cde1834a3a665831
harshmangalam <harshdev8218@gmail.com>
2024-07-09 13:24:44 +0530

    added gh constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,12 @@
+const GH_USER_API = 'https://api.github.com/user';
+const GH_ERR_REDIRECT_URI = '/account/github';
+const GH_OAUTH_URI = 'https://github.com/login/oauth/authorize';
+const GH_ACCESS_TOKEN_URI = 'https://github.com/login/oauth/access_token';
+const GH_SUCCESS_REDIRECT_URI = '/account/github/profile/';
+export {
+	GH_USER_API,
+	GH_ERR_REDIRECT_URI,
+	GH_OAUTH_URI,
+	GH_ACCESS_TOKEN_URI,
+	GH_SUCCESS_REDIRECT_URI
+};

1e260f0dab8f86f1d9c0d4c9e5285c0e24d0bbb1
Justin Maximillian Kimlim <kimlimjustin@gmail.com>
2024-07-23 01:13:47 +0700

    feat: inline-editing for tags, links and working capacity

---
+++ src/lib/components/pubpage-admin/edit-links.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	export let label: string;
+	export let url: string;
+	export let open = false;
+	export let saveCallback: (label: string, url: string) => void;
+</script>
+
+{#if open}
+	<dialog open>
+		<article>
+			<header>
+				<button aria-label="Close" rel="prev" onclick={() => (open = false)}></button>
+				<p>
+					<strong>Edit tags</strong>
+				</p>
+			</header>
+			<form>
+				<label>
+					<span>Label</span>
+					<input type="text" bind:value={label} />
+				</label>
+				<label>
+					<span>URL</span>
+					<input type="text" bind:value={url} />
+				</label>
+				<button type="submit" onclick={() => saveCallback(label, url)}>Save</button>
+				<button type="submit" class="secondary" onclick={() => saveCallback('', '')}>Delete</button>
+			</form>
+		</article>
+	</dialog>
+{/if}

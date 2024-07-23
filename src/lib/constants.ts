5f555a6de6ba76926db7461379cedf79d4a2c304
harshmangalam <harshdev8218@gmail.com>
2024-07-23 12:52:52 +0530

    added profile readme

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,7 +4,6 @@ const GH_OAUTH_URI = 'https://github.com/login/oauth/authorize';
 const GH_ACCESS_TOKEN_URI = 'https://github.com/login/oauth/access_token';
 const GH_SUCCESS_REDIRECT_PATH = '/account/github/profile/';
 const GH_ERR_REDIRECT_PATH = '/account/github';
-
 const CODEBERG_URI = `https://codeberg.org`;
 const CODEBERG_SUCCESS_REDIRECT_PATH = '/account/codeberg/profile/';
 const CODEBERG_ERR_REDIRECT_PATH = '/account/codeberg';

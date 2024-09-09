6579486677653c2a7175c98713f5a89603b7e12b
harshmangalam <harshdev8218@gmail.com>
2024-09-09 11:16:59 +0530

    added linktree lib

---
+++ src/lib/linktree.ts
@@ -0,0 +1,8 @@
+export const ICONS_MAP: { [key: string]: string } = {
+	FACEBOOK: 'mingcute:facebook-line',
+	TWITTER: 'mingcute:twitter-line',
+	INSTAGRAM: 'mdi:instagram',
+	GITHUB: 'mdi:github',
+	EMAIL_ADDRESS: 'material-symbols:mail-outline',
+	YOUTUBE: 'mingcute:youtube-line'
+};

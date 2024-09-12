85f2593153fdcf378da36cb15ed0a8bbf1f16c14
harshmangalam <harshdev8218@gmail.com>
2024-09-12 12:10:01 +0530

    remove console

--- src/routes/(app)/account/linktree/profile/+page.server.ts
+++ src/routes/(app)/account/linktree/profile/+page.server.ts
@@ -5,7 +5,6 @@ export async function load({ url }) {
 	const username = url.searchParams.get('username');
 	if (!username) throw redirect(302, '/account/linktree');
 	const { account, error } = await scrapeLinktreeAccount(username);
-	console.log(account, error);
 
 	if (error || !account) {
 		throw redirect(302, '/account/linktree');

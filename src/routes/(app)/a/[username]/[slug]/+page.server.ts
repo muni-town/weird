9b1bf43388b3659a0a0bf589b24ebbc9812fe3a4
Zicklag <zicklag@katharostech.com>
2024-09-14 20:37:22 -0500

    fix: fix username resolution for pages.

--- src/routes/(app)/a/[username]/[slug]/+page.server.ts
+++ src/routes/(app)/a/[username]/[slug]/+page.server.ts
@@ -3,14 +3,14 @@ import { appendSubpath, getMarkdownPage, profileLinkByUsername } from '$lib/leaf
 import { error } from '@sveltejs/kit';
 import { marked } from 'marked';
 import sanitizeHtml from 'sanitize-html';
-import { PUBLIC_DOMAIN } from '$env/static/public';
+import { env } from '$env/dynamic/public';
 
 export const load: PageServerLoad = async ({
 	params
 }): Promise<{ slug: string; username: string; html?: string }> => {
 	const username = params.username.includes('@')
 		? params.username.toString()
-		: `${params.username}@${PUBLIC_DOMAIN}`;
+		: `${params.username}@${env.PUBLIC_DOMAIN}`;
 	const profileLink = await profileLinkByUsername(username);
 	if (!profileLink) return error(404, `User not found: ${username}`);
 	const pageLink = appendSubpath(profileLink, params.slug);

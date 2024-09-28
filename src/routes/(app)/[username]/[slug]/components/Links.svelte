721aae9eb66b87d5a80e0de08963f47be879eea8
Joe <33972521+hnb-ku@users.noreply.github.com>
2024-09-28 21:22:41 +0400

    refactor: reorganize (app)-username-slug route. (#188)
    This is part of #187, more details can be found there.

---
+++ src/routes/(app)/[username]/[slug]/components/Links.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { PageData } from '../$types';
+	interface Props {
+		links: PageData['links'];
+	}
+
+	const { links }: Props = $props();
+</script>
+
+{#if links}
+	<h3 class="mb-4 mt-8 text-center text-2xl font-bold">Links</h3>
+	{#each links as link}
+		<a class="variant-ghost btn" target="_blank" href={link.url}>
+			{link.label || link.url}
+		</a>
+	{/each}
+{/if}

048d3fa56ceefe0742d523de223a5a5393cea27e
Joe <33972521+hnb-ku@users.noreply.github.com>
2024-10-02 20:28:55 +0400

    refactor: update (app)-feedback-confirmation to new theming pattern. (#195)
    This is part of #187, more details can be found there.

--- src/routes/(app)/feedback/confirmation/+page.svelte
+++ src/routes/(app)/feedback/confirmation/+page.svelte
@@ -1,14 +1,8 @@
 <script>
 	import { env } from '$env/dynamic/public';
+	import FeedbackPage from './componenets/FeedbackPage.svelte';
+
+	const pageTitle = `Feedback Sent | ${env.PUBLIC_INSTANCE_NAME}`;
 </script>
 
-<svelte:head>
-	<title>Feedback Sent | {env.PUBLIC_INSTANCE_NAME}</title>
-</svelte:head>
-
-<main class="flex flex-col items-center">
-	<div class="card mt-12 flex w-[600px] max-w-[90%] flex-col gap-4 p-8">
-		Your feedback was sent! Thank you for leaving a message, we may get back to you if you supplied
-		an email address.
-	</div>
-</main>
+<FeedbackPage {pageTitle} />

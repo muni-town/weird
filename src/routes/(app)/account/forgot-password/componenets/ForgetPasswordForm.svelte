f9841922fa761b931f0d3b98f620f56c4193dc90
Joe <33972521+hnb-ku@users.noreply.github.com>
2024-10-02 22:11:26 +0400

    refactor: update (app)-account-forgot-password to new theming pattern. (#197)
    This is part of #187, more details can be found there.

---
+++ src/routes/(app)/account/forgot-password/componenets/ForgetPasswordForm.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	interface props {
+		success: boolean;
+		onsubmit: (event: SubmitEvent) => void;
+	}
+
+	const { success, onsubmit }: props = $props();
+</script>
+
+<form class="card mt-12 flex w-[600px] max-w-[90%] flex-col gap-4 p-8" {onsubmit}>
+	<h1 class="my-3 text-2xl">Forgot Password</h1>
+
+	{#if success}
+		<aside class="alert variant-ghost-success">
+			<div class="alert-message">
+				<p>Your password reset email has been sent. You may now close this tab.</p>
+			</div>
+		</aside>
+	{:else}
+		<p>If you forgot your password, we can send you an email with a reset link.</p>
+		<label class="label">
+			<span>Email</span>
+			<input name="email" class="input" type="email" placeholder="Email" />
+		</label>
+
+		<button class="variant-filled btn"> Send Link</button>
+	{/if}
+</form>
---
+++ src/routes/(app)/account/forgot-password/componenets/ForgotPasswordPage.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	interface props {
+		pageTitle: string;
+		success: boolean;
+		onsubmit: (event: SubmitEvent) => void;
+	}
+
+	import ForgetPasswordForm from './ForgetPasswordForm.svelte';
+
+	const { pageTitle, success, onsubmit }: props = $props();
+</script>
+
+<svelte:head>
+	<title>{pageTitle}</title>
+</svelte:head>
+
+<main class="flex flex-col items-center">
+	<ForgetPasswordForm {success} {onsubmit} />
+</main>

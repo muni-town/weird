8e548ead73c9eb782576c84d9643cfb277e15190
Joe <33972521+hnb-ku@users.noreply.github.com>
2024-10-02 20:29:19 +0400

    refactor: update (app)-feedback to new theming pattern. (#196)
    This is part of #187, more details can be found there.

---
+++ src/routes/(app)/feedback/components/FeedbackForm.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	interface props {
+		handleSubmit: (event: SubmitEvent) => void;
+		processing: boolean;
+		error: string | null;
+		powResult: string;
+	}
+
+	import { env } from '$env/dynamic/public';
+
+	const { processing, error, handleSubmit, powResult }: props = $props();
+	const instanceName = env.PUBLIC_INSTANCE_NAME;
+</script>
+
+<form
+	class="card mt-12 flex w-[600px] max-w-[90%] flex-col gap-4 p-8"
+	method="post"
+	onsubmit={handleSubmit}
+>
+	<h1 class="my-3 text-center text-2xl">Leave Feedback or Request Help</h1>
+
+	{#if error}
+		<div class="rounded-sm bg-error-500 p-3">
+			Error submitting feedback: {error}
+		</div>
+	{/if}
+
+	<p class="text-surface-500-400-token text-center">
+		If you're having trouble or you have feedback for {instanceName} we'd appreciate a message, good
+		or bad, let us know!
+	</p>
+
+	<label class="label">
+		<span>Email <span class="text-surface-500-400-token text-sm">( optional )</span></span>
+		<input class="input" type="text" name="email" placeholder="Email" />
+		<div class="text-surface-500-400-token ml-4 text-sm">
+			If you specify an email we may get back to you.
+		</div>
+	</label>
+
+	<label class="label">
+		<span>Message</span>
+		<textarea name="content" class="textarea" placeholder="Send us a message..." rows="5">
+		</textarea>
+	</label>
+	<input type="hidden" name="pow" value={powResult} />
+
+	<button class="variant-filled btn" type="submit" disabled={processing}>
+		{processing ? 'Sending...' : 'Send'}
+	</button>
+</form>
---
+++ src/routes/(app)/feedback/components/FeedbackPage.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	interface props {
+		pageTitle: string;
+		handleSubmit: (event: SubmitEvent) => void;
+		processing: boolean;
+		error: string | null;
+		powResult: string;
+	}
+
+	import FeedbackForm from './FeedbackForm.svelte';
+
+	const { pageTitle, handleSubmit, processing, error, powResult }: props = $props();
+</script>
+
+<svelte:head>
+	<title>{pageTitle}</title>
+</svelte:head>
+
+<main class="flex flex-col items-center">
+	<FeedbackForm {handleSubmit} {processing} {error} {powResult} />
+</main>

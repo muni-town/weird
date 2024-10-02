494cc9d11a6575aa033a798f7018c03ce3671895
hnb-ku <xyz.hunabku@gmail.com>
2024-10-02 21:57:09 +0400

    refactor: update (app)-auth-v1-users-register to new theming pattern.

--- src/routes/(app)/auth/v1/users/register/+page.svelte
+++ src/routes/(app)/auth/v1/users/register/+page.svelte
@@ -4,21 +4,22 @@
 	import { env } from '$env/dynamic/public';
 	import { Pow } from '$lib/pow';
 
-	let processing = $state(false);
-	let error: string | null = $state(null);
-	let email = $state('');
-	let pow: string | undefined = $state(undefined);
+	import RegisterPage from './components/RegisterPage.svelte';
 
-	async function handleSubmit(event: SubmitEvent) {
+	let processing = $state(false);
+	let error = $state('');
+	let powResult = $state('');
+
+	async function onsubmit(event: SubmitEvent) {
 		processing = true;
 		event.preventDefault();
 		try {
 			let challenge = await get_pow_challenge();
 			console.debug('Computing proof of work');
 			console.log(challenge);
-			pow = Pow.work(challenge);
-			console.debug('Completed proof of work', pow);
-			if (!pow) {
+			powResult = Pow.work(challenge);
+			console.debug('Completed proof of work', powResult);
+			if (!powResult) {
 				error = 'Error computing proof of work.';
 				processing = false;
 				return;
@@ -30,6 +31,9 @@
 		}
 
 		try {
+			const formData = new FormData(event.target as HTMLFormElement);
+			const email = formData.get('email') as string;
+
 			const home = new URL(window.location.href);
 			home.pathname = '/my-profile';
 			const registerResp = await fetch('/auth/v1/users/register', {
@@ -40,7 +44,7 @@
 					family_name: 'User',
 					// This isn't really used, we do the redirect manually.
 					redirect_uri: home,
-					pow
+					pow: powResult
 				})
 			});
 			await checkResponse(registerResp);
@@ -52,32 +56,8 @@
 			return;
 		}
 	}
+
+	const pageTitle = `Register | ${env.PUBLIC_INSTANCE_NAME}`;
 </script>
 
-<svelte:head>
-	<title>Register | {env.PUBLIC_INSTANCE_NAME}</title>
-</svelte:head>
-
-<main class="flex flex-col items-center">
-	<form class="card mt-12 flex w-[600px] max-w-[90%] flex-col gap-4 p-8" onsubmit={handleSubmit}>
-		{#if error}
-			<div class="rounded-sm bg-error-500 p-3">
-				Error registering user: {error}
-			</div>
-		{/if}
-		<h1 class="my-3 text-2xl">Register New Account</h1>
-		<label class="label">
-			<span>Email</span>
-			<input name="email" class="input" type="text" placeholder="Email" bind:value={email} />
-		</label>
-		<input type="hidden" name="pow" value="" />
-
-		<p class="mt-4">
-			Already have an account? <a href="/my-profile" class="underline">Login</a>.
-		</p>
-
-		<button class="variant-filled btn" disabled={processing}>
-			{processing ? 'Loading...' : 'Register'}
-		</button>
-	</form>
-</main>
+<RegisterPage {pageTitle} {error} {onsubmit} {processing} />
---
+++ src/routes/(app)/auth/v1/users/register/components/RegisterPage.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	interface props {
+		pageTitle: string;
+		error: string;
+		processing: boolean;
+		onsubmit: (event: SubmitEvent) => void;
+	}
+
+	import RegisterForm from './RegisterForm.svelte';
+
+	const { pageTitle, error, onsubmit, processing }: props = $props();
+</script>
+
+<svelte:head>
+	<title>{pageTitle}</title>
+</svelte:head>
+
+<main class="flex flex-col items-center">
+	<RegisterForm {error} {onsubmit} {processing} />
+</main>

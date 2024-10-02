e6bf3dad8afdfcbf8d5a63fa392962666255791e
hnb-ku <xyz.hunabku@gmail.com>
2024-10-02 20:10:13 +0400

    DEV: refactor (app)-feedback route to new theming pattern

--- src/routes/(app)/feedback/+page.svelte
+++ src/routes/(app)/feedback/+page.svelte
@@ -2,13 +2,11 @@
 	import { env } from '$env/dynamic/public';
 	import { Pow } from '$lib/pow';
 	import { checkResponse } from '$lib/utils';
+	import FeedbackPage from './components/FeedbackPage.svelte';
 
-	let email = $state('');
-	let message = $state('');
 	let processing = $state(false);
 	let error: string | null = $state(null);
-
-	let powInput: HTMLInputElement;
+	let powResult = $state('');
 
 	async function get_pow_challenge() {
 		const resp = await fetch('/feedback/pow');
@@ -23,66 +21,26 @@
 			// TODO: add proof-of-work challenge for feedback form.
 			let challenge = await get_pow_challenge();
 			console.debug('Computing proof of work', challenge);
-			powInput.value = Pow.work(challenge) || '';
-			console.debug('Completed proof of work', powInput.value);
-			if (powInput.value == '') {
+			powResult = Pow.work(challenge) || '';
+			console.debug('Completed proof of work', powResult);
+
+			if (powResult === '') {
 				processing = false;
 				throw 'Error computing proof of work, you may need a browser update.';
 			}
-			(event.target! as HTMLFormElement).submit();
+
+			// Getting a bit of a race condition without requestAnimationFrame
+			requestAnimationFrame(() => {
+				const form = event.target as HTMLFormElement;
+				form.submit();
+			});
 		} catch (e) {
 			processing = false;
 			error = `${e}`;
 		}
 	}
+
+	const pageTitle = `Feedback | ${env.PUBLIC_INSTANCE_NAME}`;
 </script>
 
-<svelte:head>
-	<title>Feedback | {env.PUBLIC_INSTANCE_NAME}</title>
-</svelte:head>
-
-<main class="flex flex-col items-center">
-	<form
-		class="card mt-12 flex w-[600px] max-w-[90%] flex-col gap-4 p-8"
-		method="post"
-		onsubmit={handleSubmit}
-	>
-		<h1 class="my-3 text-center text-2xl">Leave Feedback or Request Help</h1>
-
-		{#if error}
-			<div class="rounded-sm bg-error-500 p-3">
-				Error submitting feedback: {error}
-			</div>
-		{/if}
-
-		<p class="text-surface-500-400-token text-center">
-			If you're having trouble or you have feedback for {env.PUBLIC_INSTANCE_NAME} we'd appreciate a
-			message, good or bad, let us know!
-		</p>
-
-		<label class="label">
-			<span>Email <span class="text-surface-500-400-token text-sm">( optional )</span></span>
-			<input class="input" type="text" name="email" placeholder="Email" bind:value={email} />
-			<div class="text-surface-500-400-token ml-4 text-sm">
-				If you specify an email we may get back to you.
-			</div>
-		</label>
-
-		<label class="label">
-			<span>Message</span>
-			<textarea
-				name="content"
-				class="textarea"
-				placeholder="Send us a message..."
-				rows="5"
-				bind:value={message}
-			>
-			</textarea>
-		</label>
-		<input type="hidden" name="pow" bind:this={powInput} />
-
-		<button class="variant-filled btn" type="submit" disabled={processing}>
-			{processing ? 'Sending...' : 'Send'}
-		</button>
-	</form>
-</main>
+<FeedbackPage {handleSubmit} {pageTitle} {processing} {error} {powResult} />

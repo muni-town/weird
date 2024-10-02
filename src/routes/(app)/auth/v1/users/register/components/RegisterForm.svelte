962e50f5541a07251e6fe9bb2dad4a62af7bab4a
Joe <33972521+hnb-ku@users.noreply.github.com>
2024-10-02 22:11:08 +0400

    refactor: update (app)-auth-v1-users-register to new theming pattern. (#198)
    This is part of #187, more details can be found there.

---
+++ src/routes/(app)/auth/v1/users/register/components/RegisterForm.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	interface props {
+		error: string;
+		processing: boolean;
+		onsubmit: (event: SubmitEvent) => void;
+	}
+
+	const { error, processing, onsubmit }: props = $props();
+</script>
+
+<form class="card mt-12 flex w-[600px] max-w-[90%] flex-col gap-4 p-8" {onsubmit}>
+	{#if error}
+		<div class="rounded-sm bg-error-500 p-3">
+			Error registering user: {error}
+		</div>
+	{/if}
+	<h1 class="my-3 text-2xl">Register New Account</h1>
+	<label class="label">
+		<span>Email</span>
+		<input name="email" class="input" type="text" placeholder="Email" />
+	</label>
+	<input type="hidden" name="pow" value="" />
+
+	<p class="mt-4">
+		Already have an account? <a href="/my-profile" class="underline">Login</a>.
+	</p>
+
+	<button class="variant-filled btn" disabled={processing}>
+		{processing ? 'Loading...' : 'Register'}
+	</button>
+</form>

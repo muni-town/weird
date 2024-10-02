39c409b18251c9d9f7e44dafbc9afd2db947b9d2
hnb-ku <xyz.hunabku@gmail.com>
2024-10-02 19:24:51 +0400

    DEV: refactor (app)-feedback-confirmation to new theming pattern

---
+++ src/routes/(app)/feedback/confirmation/componenets/FeedbackPage.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	interface props {
+		pageTitle: string;
+	}
+
+	const { pageTitle }: props = $props();
+</script>
+
+<svelte:head>
+	<title>{pageTitle}</title>
+</svelte:head>
+
+<main class="flex flex-col items-center">
+	<div class="card mt-12 flex w-[600px] max-w-[90%] flex-col gap-4 p-8">
+		Your feedback was sent! Thank you for leaving a message, we may get back to you if you supplied
+		an email address.
+	</div>
+</main>

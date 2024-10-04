84e48979a173e9fe971bfbbafed3704f471946e0
Zicklag <zicklag@katharostech.com>
2024-10-04 14:37:51 -0500

    fix: remove an unused avatar route.

--- src/routes/(app)/account/[user_id]/avatar/+server.ts
+++
@@ -1,9 +0,0 @@
-import { createImageResponse } from '$lib/image';
-import { profileLinkById } from '$lib/leaf/profile';
-import { error, type RequestHandler } from '@sveltejs/kit';
-
-export const GET: RequestHandler = async ({ fetch, params }) => {
-	const profileLink = await profileLinkById(params.user_id!);
-	if (!profileLink) return error(404, 'Avatar not found');
-	return createImageResponse(profileLink, fetch, params.user_id);
-};

bb20d083fa98f3513f9c257955eaff78ddb3c8fe
Zicklag <zicklag@katharostech.com>
2024-11-05 09:16:26 -0600

    fix: serve NS and SOA records from the DNS server.

--- src/lib/dns/server.ts
+++ src/lib/dns/server.ts
@@ -8,7 +8,7 @@ import * as network from 'dinodns/common/network';
 import type { SupportedAnswer } from 'dinodns/types/dns';
 import { DefaultStore } from 'dinodns/plugins/storage';
 import { dev } from '$app/environment';
-import { AUTHORITATIVE_ANSWER } from 'dns-packet';
+import { AUTHENTIC_DATA, AUTHORITATIVE_ANSWER, RECURSION_AVAILABLE } from 'dns-packet';
 import { z } from 'zod';
 
 const REDIS_USER_PREFIX = 'weird:users:';
@@ -33,6 +33,11 @@ const WEIRD_HOST_A_RECORD_REGEX = new RegExp(
 );
 
 const DNS_PORT = parseInt(env.DNS_PORT || '53');
+const APP_IPS = env.APP_IPS.split(',');
+const DNS_MASTER = env.DNS_SOA_MASTER;
+const soaSplit = env.DNS_SOA_EMAIL.split('@');
+const DNS_EMAIL = soaSplit[0].replace('.', '\\.') + '.' + soaSplit[1];
+const DNS_NAMESERVERS = env.DNS_NAMESERVERS.split(',');
 
 /**
  * Start the Weird DNS server and return the `Redis` store with the mapping from username
@@ -56,107 +61,162 @@ export async function startDnsServer() {
 	// Because Weird is both the DNS server and the app server, we look up
 	// the NS ( nameserver ) records associated to our public domain.
 	const appDomain = pubenv.PUBLIC_DOMAIN.split(':')[0];
-	const selfIps = pubenv.PUBLIC_IPS.split(',');
 
 	// Now we can add an A record that will direct web traffic to the app
-	staticRecords.set(appDomain, 'A', selfIps);
+	staticRecords.set(appDomain, 'A', APP_IPS);
 	s.use(staticRecords.handler);
+
+	s.use(async (req, res, next) => {
+		if (res.finished) return next();
+
+		if (req.packet.questions.length > 1) {
+			console.warn('Returning not implemented for DNS queries with multiple questions.');
+			res.errors.notImplemented();
+		} else {
+			next();
+		}
+	});
+
+	// Set all answers to authoritative by default
+	s.use(async (_req, res, next) => {
+		if (res.finished) return next();
+
+		res.packet.flags = res.packet.flags | AUTHORITATIVE_ANSWER;
+		next();
+	});
+
+	// Return SOA responses
+	s.use(async (req, res, next) => {
+		if (res.finished) return next();
+
+		const question = req.packet.questions[0];
+		if (question.type == 'SOA') {
+			if (question.name.endsWith(pubenv.PUBLIC_DOMAIN)) {
+				res.packet.flags = res.packet.flags | AUTHENTIC_DATA;
+				return res.answer({
+					type: 'SOA',
+					name: pubenv.PUBLIC_DOMAIN,
+					data: {
+						mname: DNS_MASTER,
+						rname: DNS_EMAIL
+					}
+				});
+			} else {
+				return res.errors.refused();
+			}
+		}
+
+		next();
+	});
+
+	// Return NS responses
+	s.use(async (req, res, next) => {
+		if (res.finished) return next();
+
+		const question = req.packet.questions[0];
+		if (question.type == 'NS') {
+			if (question.name == pubenv.PUBLIC_DOMAIN) {
+				res.packet.flags = res.packet.flags | AUTHENTIC_DATA;
+				return res.answer(
+					DNS_NAMESERVERS.map((ns) => ({
+						type: 'NS',
+						name: question.name,
+						data: ns
+					}))
+				);
+			} else {
+				return res.errors.refused();
+			}
+		}
+
+		next();
+	});
 
 	// Resolve records stored in Redis
 	s.use(async (req, res, next) => {
-		const results = (await Promise.all(
-			req.packet.questions.map(
-				(question) =>
-					new Promise(async (ret) => {
-						const returnAnswers = (v: any) => {
-							if (v) {
-								res.packet.flags = res.packet.flags | AUTHORITATIVE_ANSWER;
-							}
-							ret(v);
-						};
-						const { type, name } = question;
-						const redisKey = REDIS_DNS_RECORD_PREFIX + type + ':' + name;
-						let record;
-						try {
-							const extraRecords: { name: string; type: string; data: string; ttl?: number }[] = [];
+		if (res.finished) return next();
 
-							// If this is an A record query, we also need to check for CNAME
-							// records.
-							if (type == 'A') {
-								const redisCnameKey = REDIS_DNS_RECORD_PREFIX + 'CNAME:' + name;
-								record = await redis.get(redisCnameKey);
-								if (record) {
-									try {
-										const parsed = redisDnsRecordSchema.parse(JSON.parse(record));
-										extraRecords.push(
-											...parsed.map((r) => ({
-												name,
-												type: 'CNAME',
-												data: r.data,
-												ttl: r.ttl
-											}))
-										);
-										const aRecordIps = (
-											await Promise.all(
-												parsed.map(
-													(record) =>
-														new Promise((ret) => {
-															dns.resolve(record.data, (err, addrs) => {
-																console.log(err);
-																if (!err) {
-																	ret(
-																		addrs.map((ip) => ({
-																			name: record.data,
-																			type: 'A',
-																			data: ip
-																		}))
-																	);
-																}
-															});
-														})
-												)
-											)
-										).flat() as { name: string; type: 'A'; data: string }[];
-										extraRecords.push(...aRecordIps);
-									} catch (e) {
-										console.warn('Error parsing DNS record from redis:', redisKey, record, e);
-									}
-								}
-							}
-
-							record = await redis.get(redisKey);
-							if (!record) return returnAnswers(extraRecords);
-							const parsed = redisDnsRecordSchema.parse(JSON.parse(record));
-							returnAnswers([
-								...extraRecords,
-								...parsed.map((record) => ({
+		const question = req.packet.questions[0];
+		const { type, name } = question;
+		let record;
+		// If this is an A record query, we also need to check for CNAME
+		// records.
+		if (type == 'A') {
+			const redisKey = REDIS_DNS_RECORD_PREFIX + 'CNAME:' + name;
+			record = await redis.get(redisKey);
+			if (record) {
+				try {
+					const parsed = redisDnsRecordSchema.parse(JSON.parse(record));
+					req.packet.answers = [
+						...req.packet.answers,
+						...parsed.map(
+							(r) =>
+								({
 									name,
-									type,
-									data: record.data,
-									ttl: record.ttl
-								}))
-							]);
-						} catch (e) {
-							console.warn('Error parsing DNS record from redis:', redisKey, record, e);
-							returnAnswers(null);
-						}
-					})
-			)
-		)) as (SupportedAnswer[] | null)[];
-
-		// Return answers
-		const filtered = results
-			.filter((x) => !!x)
-			.map((x) => x as unknown as SupportedAnswer)
-			.flat();
-		if (filtered.length > 0) res.answer(filtered);
+									type: 'CNAME',
+									data: r.data,
+									ttl: r.ttl
+								}) as SupportedAnswer
+						)
+					];
+					await Promise.all(
+						parsed.map(
+							(record) =>
+								new Promise((done) => {
+									dns.resolve(record.data, (err, addrs) => {
+										console.error('Error looking up A record for cname', record.data, err);
+										if (err) return done(null);
+										res.packet.answers = [
+											...req.packet.answers,
+											...addrs.map(
+												(ip) =>
+													({
+														name: record.data,
+														type: 'A',
+														data: ip
+													}) as SupportedAnswer
+											)
+										];
+										done(null);
+									});
+								})
+						)
+					);
+					res.resolve();
+				} catch (e) {
+					console.warn('Error parsing DNS record from redis:', redisKey, record, e);
+				}
+			} else {
+				// If there is not a CNAME record at for this domain, check for A records
+				const redisKey = REDIS_DNS_RECORD_PREFIX + type + ':' + name;
+				record = await redis.get(redisKey);
+				if (record) {
+					try {
+						const parsed = redisDnsRecordSchema.parse(JSON.parse(record));
+						res.answer(
+							parsed.map(
+								(record) =>
+									({
+										name,
+										type,
+										data: record.data,
+										ttl: record.ttl
+									}) as SupportedAnswer
+							)
+						);
+					} catch (e) {
+						console.warn('Error parsing DNS record from redis:', redisKey, record, e);
+					}
+				}
+			}
+		}
 
 		next();
 	});
 
 	// Resolve records for registered users
 	s.use(async (req, res, next) => {
-		if (res.packet.answers.length > 0) return next();
+		if (res.finished) return next();
 
 		const results = (await Promise.all(
 			req.packet.questions.map(
@@ -191,7 +251,7 @@ export async function startDnsServer() {
 								// if (!exists) return returnAnswers(null);
 
 								returnAnswers(
-									selfIps.map((ip) => ({
+									APP_IPS.map((ip) => ({
 										name,
 										type,
 										data: ip,
@@ -225,10 +285,9 @@ export async function startDnsServer() {
 
 		// Add middleware that will respond with forwarded requests
 		s.use(async (req, res, next) => {
-			// If there's already an answer, we don't need to do anything
-			if (res.packet.answers.length > 0) {
-				return;
-			}
+			// If there's already an answer, continue
+			if (res.finished) return next();
+
 			const resolver = new dns.Resolver();
 			resolver.setServers(defaultDnsServers);
 

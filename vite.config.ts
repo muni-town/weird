4b1bf8de1ec180dd107e11f571bd296ca9e16cac
Zicklag <zicklag@katharostech.com>
2024-11-26 19:44:55 -0600

    build: attempt to fix production build by esm-ifying CommonJS modules.

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
+import commonjs from '@rollup/plugin-commonjs';
 
 import { defineConfig, searchForWorkspaceRoot } from 'vite';
 
 export default defineConfig({
-	plugins: [wasm(), topLevelAwait(), sveltekit()],
+	plugins: [commonjs(), wasm(), topLevelAwait(), sveltekit()],
 	server: {
 		host: '0.0.0.0',
 		port: 9523,

47851ab28a638a673f3447d112f7363be60be1c8
Zicklag <zicklag@katharostech.com>
2024-12-17 19:56:25 -0600

    fix: fix "login with Weird".
    Makes sure that the query parameters are added to the /login route when redirecting from /auth/v1/oidc/authorize.

--- src/routes/(app)/auth/v1/oidc/authorize/+server.ts
+++ src/routes/(app)/auth/v1/oidc/authorize/+server.ts
@@ -1,8 +1,8 @@
 import { proxy_to_rauthy } from '$lib/rauthy/server';
 import { redirect, type RequestHandler } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async () => {
-	return redirect(303, '/login');
+export const GET: RequestHandler = async ({ url }) => {
+	return redirect(303, `/login${url.search}${url.hash}`);
 };
 
 export const fallback = proxy_to_rauthy;

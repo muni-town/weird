42fb7f616d7831eb31154444c60bf4104addf41b
Zicklag <zicklag@katharostech.com>
2025-04-15 11:10:49 -0500

    feat: add config to hide registration form.

--- src/routes/(app)/auth/v1/users/register/components/RegisterForm.svelte
+++ src/routes/(app)/auth/v1/users/register/components/RegisterForm.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { env } from '$env/dynamic/public';
+
 	interface props {
 		error: string;
 		processing: boolean;
@@ -9,23 +11,30 @@
 </script>
 
 <form class="card mt-12 flex w-[600px] max-w-[90%] flex-col gap-4 p-8" {onsubmit}>
-	{#if error}
-		<div class="rounded-sm bg-error-500 p-3">
-			Error registering user: {error}
+	{#if env.PUBLIC_HIDE_REGISTRATION == 'true'}
+		<div class="flex flex-col items-center gap-4 text-center text-xl">
+			<p>Registration is temporarily disabled.</p>
+			<p>We will be back online soon!</p>
 		</div>
+	{:else}
+		{#if error}
+			<div class="rounded-sm bg-error-500 p-3">
+				Error registering user: {error}
+			</div>
+		{/if}
+		<h1 class="my-3 text-2xl">Register New Account</h1>
+		<label class="label">
+			<span>Email</span>
+			<input name="email" class="input" type="text" placeholder="Email" />
+		</label>
+		<input type="hidden" name="pow" value="" />
+
+		<p class="mt-4">
+			Already have an account? <a href="/my-profile" class="underline">Login</a>.
+		</p>
+
+		<button class="variant-filled btn" disabled={processing}>
+			{processing ? 'Loading...' : 'Register'}
+		</button>
 	{/if}
-	<h1 class="my-3 text-2xl">Register New Account</h1>
-	<label class="label">
-		<span>Email</span>
-		<input name="email" class="input" type="text" placeholder="Email" />
-	</label>
-	<input type="hidden" name="pow" value="" />
-
-	<p class="mt-4">
-		Already have an account? <a href="/my-profile" class="underline">Login</a>.
-	</p>
-
-	<button class="variant-filled btn" disabled={processing}>
-		{processing ? 'Loading...' : 'Register'}
-	</button>
 </form>
